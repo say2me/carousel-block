907888e1491ce28bbd4d6e0b76939919adb388da
Vjaceslavs Vasenins <time2type@gmail.com>
2022-02-12 15:01:44 +0200

    Initial commit

---
+++ carousel/src/edit.js
@@ -0,0 +1,59 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit() {
+	const blockProps = useBlockProps( {
+		className: 'xwp-carousel',
+	} );
+
+	const addNewSlide = () => {
+
+	}
+	return (
+		<div { ...useBlockProps() }>
+			<div className="xwp-slide-container">
+			<InnerBlocks
+					allowedBlocks={ [ 'xwpblocks/slide' ] }
+					template={ [ [ 'xwpblocks/slide', {} ] ] }
+				/>
+			</div>
+			<div className="xwp-arrow xwp-back xwp-back-button">←</div>
+        	<div class="xwp-arrow xwp-forward xwp-forward-button">→</div>
+			<button
+					type="button"
+					onClick={ addNewSlide }
+					className="xwp-blocks-carousel-add-new-slide-button"
+				>
+					{ __( 'Add a new slide', 'xwp-blocks' ) }
+				</button>
+		</div>
+	);
+}
